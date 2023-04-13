7aa74f3de0c1d91af58f7a96a0233c98e213b3dc
scyllacore <yms8795@gmail.com>
2023-04-13 16:19:26 +0900

    test : test 코드 추가.

---
+++ Practice/JBlogWeb/src/test/java/com/scyllacore/jblogweb/UserDAOTest.java
@@ -0,0 +1,30 @@
+package com.scyllacore.jblogweb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.scyllacore.jblogweb.domain.User;
+import com.scyllacore.jblogweb.persistence.UserDAO;
+
+@SpringBootTest
+public class UserDAOTest {
+    @Autowired
+    private UserDAO userDAO;
+
+    @Test
+    void getUserListTest(){
+        User user = new User();
+        user.setUserName("test");
+        user.setPassword("123");
+        user.setEmail("test@gmail");
+
+        int before = userDAO.getUserList().size();
+        userDAO.insertUser(user);
+        int after = userDAO.getUserList().size();
+
+        assertEquals(before+1,after);
+    }
+}
