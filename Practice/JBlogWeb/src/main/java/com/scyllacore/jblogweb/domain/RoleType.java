1db4f4ec2f9052fc2818e5c5693e9c7f790a7faf
scyllacore <yms8795@gmail.com>
2023-04-19 12:58:33 +0900

    feat : 3.2.4 구현.

---
+++ Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/domain/RoleType.java
@@ -0,0 +1,5 @@
+package com.scyllacore.jblogweb.domain;
+
+public enum RoleType {
+    USER,ADMIN;
+}
--- Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/domain/User.java
+++ Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/domain/User.java
@@ -1,18 +1,42 @@
 package com.scyllacore.jblogweb.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
 import lombok.Builder; // import 순서 맞출 것. 컨벤션에 따라 코드를 적는 순서에 맞췄다
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Entity
+@Table(name = "USERS")
 public class User {
+    @Id
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(nullable = false, length = 50, unique = true)
     private String userName;
+
+    @Column(length = 100)
     private String password;
+
+    @Column(nullable = false, length = 100)
     private String email;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType role;
+
+    @CreationTimestamp
+    private Time createDate;
+
 }
