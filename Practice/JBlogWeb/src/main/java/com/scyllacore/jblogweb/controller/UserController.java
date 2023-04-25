4e254fda07e5d3e845b5295a92263f08a3f92886
scyllacore <yms8795@gmail.com>
2023-04-25 17:00:42 +0900

    feat : 3.3.5 구현.

--- Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/controller/UserController.java
+++ Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/controller/UserController.java
@@ -4,28 +4,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import com.scyllacore.jblogweb.exception.JBlogException;
 import org.springframework.transaction.annotation.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+
 import com.scyllacore.jblogweb.domain.RoleType;
 import com.scyllacore.jblogweb.domain.User;
 import com.scyllacore.jblogweb.persistence.UserRepository;
+
 
 @Controller
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    //@Transactional -> save를 자동으로 해줌
+   /* @GetMapping("/user/page/{page}")
+    public @ResponseBody Page<User> getUserListPaging(@PathVariable int page){
+        Pageable pageable =
+                PageRequest.of(page,2,Sort.Direction.DESC,"id","userName");
+
+        return userRepository.findAll(pageable);
+    }*/
+
+    @GetMapping("/user/page/{page}")
+    public @ResponseBody Page<User> getUserListPaging(
+            @PageableDefault(page = 0, size = 2, direction = Sort.Direction.DESC, sort = {"id", "userName"})
+            Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @GetMapping("/user/list")
+    public @ResponseBody List<User> getUserList() {
+        return userRepository.findAll();
+    }
 
     @DeleteMapping("/user/{id}")
-    public @ResponseBody String deleteUser(@PathVariable int id){
+    public @ResponseBody String deleteUser(@PathVariable int id) {
         userRepository.deleteById(id);
         return "회원 삭제 성공";
     }
 
+    //@Transactional -> save를 자동으로 해줌
     @PutMapping("/user")
     public @ResponseBody String updateUser(@RequestBody User user) {
         User findUser = userRepository.findById(user.getId()).orElseThrow(() -> {
