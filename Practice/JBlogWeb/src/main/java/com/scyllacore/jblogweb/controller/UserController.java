3ebbc71b6b91b84082cf688871f1568422020bc9
scyllacore <yms8795@gmail.com>
2023-04-25 16:17:33 +0900

    feat : 3.3.3 구현.

--- Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/controller/UserController.java
+++ Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/controller/UserController.java
@@ -2,15 +2,12 @@ package com.scyllacore.jblogweb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.function.Supplier;
+
 import com.scyllacore.jblogweb.exception.JBlogException;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.scyllacore.jblogweb.domain.RoleType;
 import com.scyllacore.jblogweb.domain.User;
@@ -21,8 +18,24 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    //@Transactional -> save를 자동으로 해줌
+    @PutMapping("/user")
+    public @ResponseBody String updateUser(@RequestBody User user) {
+        User findUser = userRepository.findById(user.getId()).orElseThrow(() -> {
+            return new JBlogException(user.getId() + "번 회원은 없습니다");
+        });
+
+        findUser.setUserName(user.getUserName());
+        findUser.setPassword(user.getPassword());
+        findUser.setEmail(user.getEmail());
+
+        userRepository.save(findUser);
+
+        return "회원 정보 수정 성공";
+    }
+
     @GetMapping("/user/get/{id}")
-    public @ResponseBody User getUser(@PathVariable int id){
+    public @ResponseBody User getUser(@PathVariable int id) {
         //존재하지 않는 회원 조회 시 예외 반환.
 
         User findUser = userRepository.findById(id).orElseThrow(new Supplier<JBlogException>() {
@@ -36,7 +49,7 @@ public class UserController {
 
 
     @PostMapping("/user")
-    public @ResponseBody String insertUser(@RequestBody User user){
+    public @ResponseBody String insertUser(@RequestBody User user) {
         user.setRole(RoleType.USER);
         userRepository.save(user);
         return user.getUserName() + " 회원가입 성공";
