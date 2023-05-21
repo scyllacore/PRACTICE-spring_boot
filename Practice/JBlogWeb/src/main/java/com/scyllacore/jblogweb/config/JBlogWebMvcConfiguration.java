59760627a37b29016677372bc9e943fa8e3c8685
scyllacore <yms8795@gmail.com>
2023-05-21 17:17:20 +0900

    feat : 6.2.2까지 구현.

--- Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/config/JBlogWebMvcConfiguration.java
+++ Practice/JBlogWeb/src/main/java/com/scyllacore/jblogweb/config/JBlogWebMvcConfiguration.java
@@ -1,11 +1,18 @@
 package com.scyllacore.jblogweb.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class JBlogWebMvcConfiguration implements WebMvcConfigurer {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry){
         registry.addInterceptor(
